b3f0cddf4c3036d31078b8cb6bfbb1cbcf167b2c
SowmyaKurapati13 <134614410+SowmyaKurapati13@users.noreply.github.com>
2023-07-11 12:08:54 +0530

    Update HumanResourceAppApplicationTests.java

--- src/test/java/com/example/hra/HumanResourceAppApplicationTests.java
+++ src/test/java/com/example/hra/HumanResourceAppApplicationTests.java
@@ -3,10 +3,10 @@ package com.example.hra;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class HumanResourceAppApplicationTests {
 
-    @Test
+  //  @Test
     void contextLoads() {
     }
 
